031e5f6af0d125bd8c35b49f804a0fc6ce3a7db1
devdonatti <mdonattiprogramacion@gmail.com>
2024-08-22 12:58:20 -0300

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",        // Incluye el archivo HTML raíz
+    "./src/**/*.{js,ts,jsx,tsx}", // Si tienes archivos JavaScript o TypeScript en `src`, inclúyelos aquí
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
